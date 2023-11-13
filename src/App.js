ba784cd35f92a51948597c1b6965d7e1a245eafc
Taesunpark <tovbskvb@daum.net>
2023-11-13 17:10:10 +0900

    [ADD] props 활용해서 modal안에 있는 버튼 누르면 첫번째 list의 본문 헤드 내용 바뀌게 설정

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App(){
   let post = '강남 우동 맛집';
   let [글제목, b] = useState(['남자 코트 추천', '여자 코트 추천', '맛집 추천']);  
   let [따봉, 따봉변경] =  useState([0,0,0]);
+  let [modal, 모달변경] = useState(false);
   // UI의 현재 상태를 state로 저장
-  // let [modal, setModal] = useState(false);
+  
   
   // array/object 특징
   // array/object 담은 변수엔 화살표만 저장됨
@@ -19,10 +20,11 @@ function App(){
     <div className="App">      
       <div className = "black-nav">
         <h4 style = {{color:'red', fontSize: '20px'}}>{글제목[0]}</h4>  
-      </div>            
+      </div>                  
 
       {
-        글제목.map(function(a, i){          
+        글제목.map(function(a, i){
+
           return (          
             <div className="list">              
               <h4>{i + 1} {a} {따봉[i]}</h4>              
@@ -34,10 +36,19 @@ function App(){
               }}>카운트 업</button>
               <p>날짜</p>
               <p>상세내용</p>
-            </div>
+              <button onClick={
+                () => {                  
+                  모달변경(!modal);                  
+                }
+              }>모달</button>                   
+            </div>            
           )
         })
-      }
+      }    
+
+      {
+        modal == true ? <Modal color = "yellow" 글제목 = {글제목} 첫번째헤드 = {b}></Modal> : null
+      }        
   
     </div>
   );
@@ -52,12 +63,19 @@ function App(){
 // 단점
 // 1. state 가져다쓸 때 문제생김
 
-function Modal(){
+function Modal(props){
   return(    
-      <div className="modal">
-        <h4>제목11</h4>
+      <div className="modal" style={{background : props.color}}>
+        <h4>{props.글제목[0]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+        <button onClick ={
+          () => {
+            let copy = [... props.글제목];
+            copy[0] = "여자코트 추천";
+            props.첫번째헤드(copy);
+          }
+        }>글수정</button>
       </div>      
   )
 }
