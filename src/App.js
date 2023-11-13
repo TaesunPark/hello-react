3a7cd2f0b13db7d91460a222ff0feba6c934caf8
Taesunpark <tovbskvb@daum.net>
2023-11-13 13:29:38 +0900

    [ADD] button 클릭 시 Modal 창 띄우는 기능 추가, 해당 기능을 useState로 상태 관리

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App(){
   let post = '강남 우동 맛집';
   let [글제목, b] = useState(['남자 코트 추천', '여자 코트 추천', '맛집 추천']);  
   let [따봉, 따봉변경] =  useState(0);
+  // UI의 현재 상태를 state로 저장
+  let [modal, setModal] = useState(false);
   
   // array/object 특징
   // array/object 담은 변수엔 화살표만 저장됨
@@ -41,12 +43,44 @@ function App(){
       </div>
 
       <div className="list">
-        <h4>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
+        <h4>{글제목[2]}</h4>        
+        <p>2월 18일 발행</p>
       </div>
-      
+
+      <button onClick={()=>{         
+        setModal(!modal);
+        }}></button>
+
+      {
+        modal == true ? <Modal></Modal> : null
+      } 
+
     </div>
   );
 }
+
+// const 변수는 에러 메시지를 띄워줌
+
+// 컴포넌트로 만들면 좋은 경우
+// 1. 반복적인 html 축약할 때
+// 2. 큰 페이지들
+// 3. 자주변경되는 것들
+// 단점
+// 1. state 가져다쓸 때 문제생김
+
+function Modal(){
+  return(    
+      <div className="modal">
+        <h4>제목11</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>      
+  )
+}
+
+// [동적인 UI 만드는 step]
+// 1. html css로 미리 디자인 완성
+// 2. UI의 현재 상태를 state로 저장
+// 3. state에 따라 UI가 어떻게 보일지 작성
 
 export default App;
