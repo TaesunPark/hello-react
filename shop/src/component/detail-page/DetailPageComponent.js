5fe1f31ffff53aa2add79d5e67c7952994b70cee
Taesunpark <tovbskvb@daum.net>
2023-11-15 18:50:17 +0900

    css Component 사용

--- shop/src/component/detail-page/DetailPageComponent.js
+++ shop/src/component/detail-page/DetailPageComponent.js
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
-
+import styled from 'styled-components'
 
 function DetailPageComponet(props) {
 
     let {id} = useParams();    
     let img_dir = "https://github.com/codingapple1/codingapple1.github.io/blob/master/shop/shoes" + (Number(id) + 1);
-    
 
+    let YellowBtn = styled.button`
+        background : yellow;
+        color : black;
+        padding : 10px;
+    `
+    
     return (
         <div className="container">
             <div className="row">
@@ -19,6 +24,7 @@ function DetailPageComponet(props) {
                     <p>{props.shoes[id].price}</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
+                <YellowBtn>주문</YellowBtn>
             </div>
         </div>
     );
