e79804d6b75dbdd3c1d2007e72ffb16fdc027974
Taesunpark <tovbskvb@daum.net>
2023-11-16 11:14:42 +0900

    [ADD] detail 페이지 들어가면 2초 뒤에 할인 컴포넌트 사라지게 설정, useEffect 사용

--- shop/src/component/component.js
+++ shop/src/component/component.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Item(props) {    
-    const img_dir = "https://github.com/codingapple1/codingapple1.github.io/blob/master/shop/shoes";        
-    
+    const img_dir = "https://github.com/codingapple1/codingapple1.github.io/blob/master/shop/shoes";            
+    let dir = "detail/" + (props.idx.id)
     return (
-        <div>                                    
-            <img src={img_dir + (props.idx.id + 1) + '.jpg?raw=true'} width="80%" />
+        
+        <div>
+            <Link to = {dir}>
+                <img src={img_dir + (props.idx.id + 1) + '.jpg?raw=true'} width="80%" />
+            </Link>                                    
             <h4>{props.idx.title}</h4>
             <p>{props.idx.content}</p>
             <p>{props.idx.price}</p>
--- shop/src/component/detail-page/DetailPageComponent.js
+++ shop/src/component/detail-page/DetailPageComponent.js
@@ -1,27 +1,47 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from 'styled-components'
 
-function DetailPageComponet(props) {
+function DetailPageComponent(props) {
 
-    let {id} = useParams();    
+    let [count, setCount] = useState(0);
+    let { id } = useParams();
     let img_dir = "https://github.com/codingapple1/codingapple1.github.io/blob/master/shop/shoes" + (Number(id) + 1);
+    let [isVisible, setIsVisible] = useState(true);
+
+    let 찾은상품 = props.shoes.find(function (x) {
+        return x.id == id;
+    });
 
     let YellowBtn = styled.button`
         background : yellow;
         color : black;
         padding : 10px;
     `
-    
+
+    // html 렌더링 후에 동작함.
+    useEffect(() => {
+        setTimeout(() => {
+            setIsVisible(false);
+        }, 2000);
+    })
+
     return (
         <div className="container">
+
+            <SaleComponent isVisible = {isVisible}/>
+
+            {count}
+            <button onClick={() => { setCount(count + 1) }}>버튼{count}</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src={img_dir + '.jpg?raw=true'} width="100%" />
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">{props.shoes[id].title}</h4>
-                    <p>{props.shoes[id].content}</p>
-                    <p>{props.shoes[id].price}</p>
+                    <h4 className="pt-5">{찾은상품.title}</h4>
+                    <p>{찾은상품.content}</p>
+                    <p>{찾은상품.price}</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
                 <YellowBtn>주문</YellowBtn>
@@ -30,4 +50,13 @@ function DetailPageComponet(props) {
     );
 }
 
-export default DetailPageComponet;
+function SaleComponent(props) {
+    return (
+        props.isVisible &&
+        <div className="alert alert-warning">
+            2초이내 구매시 할인
+        </div>    
+    )
+}
+
+export default DetailPageComponent;
